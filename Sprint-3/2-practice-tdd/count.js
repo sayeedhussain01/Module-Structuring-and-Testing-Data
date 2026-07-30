7ba6713443d3738446ee0ce12f63c068f866f2e5
Sahid Hussain <hussainsahid980@gmail.com>
2026-07-30 14:04:41 +0100

    restore all file

--- Sprint-3/2-practice-tdd/count.js
+++ Sprint-3/2-practice-tdd/count.js
@@ -1,13 +1,5 @@
 function countChar(stringOfCharacters, findCharacter) {
-  // return 5;
-
-  let count = 0;
-  for (let char of stringOfCharacters) {
-    if (char === findCharacter) {
-      count++;
-    }
-  }
-
-  return count;
+  return 5
 }
+
 module.exports = countChar;
